de81ba5d7c75409d5c9cc650ccabc836bfbc24c2
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2016-12-23 12:58:41 -0800

    Add rails resources for virtual pet game feature

--- app/assets/javascripts/virtualpet.js
+++ app/assets/javascripts/virtualpet.js
@@ -19,7 +19,18 @@ var GameState = {
   },
 
   create: function(){
+    this.background = this.game.add.sprite(0, 0, 'backyard');
 
+    this.pet = this.game.add.sprite(100, 400, 'pet');
+    this.pet.anchor.setTo(0.5);
+
+    //custom properties - not built in phaser methods
+    this.pet.customParams = {health: 100, fun: 100};
+
+    this.apple = this.game.add.sprite(72, 570, 'apple');
+    this.candy = this.game.add.sprite(144, 570, 'candy');
+    this.toy = this.game.add.sprite(216, 570, 'toy');
+    this.rotate = this.game.add.sprite(288, 570, 'rotate');
   },
 };
 
