be3d32cc0a2a9b7b5b3a248f3e127ef7affc58f9
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2016-12-23 14:38:05 -0800

    Resize assets for flappy bird clone

--- app/assets/javascripts/fbird.js
+++ app/assets/javascripts/fbird.js
@@ -1,53 +1,118 @@
 function fBird(){
 
 var mainState = {
-  preload: function() {
-    // Load the bird sprite
-    game.load.image('bird', 'assets/fbird/bird.png');
-    game.load.image('pipe', 'assets/fbird/pipe.png');
-  },
 
-  create: function() {
-    // Change the background color of the game to blue
-    game.stage.backgroundColor = '#71c5cf';
+    preload: function() {
+        if(!game.device.desktop) {
+            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            game.scale.setMinMax(game.width/2, game.height/2, game.width, game.height);
+        }
 
-    // Set the physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
 
-    // Display the bird at the position x=100 and y=245
-    this.bird = game.add.sprite(100, 245, 'bird');
+        game.stage.backgroundColor = '#71c5cf';
 
-    // Add physics to the bird
-    // Needed for: movements, gravity, collisions, etc.
-    game.physics.arcade.enable(this.bird);
+        game.load.image('bird', 'assets/fbird/bird.png');
+        game.load.image('pipe', 'assets/fbird/pipe.png');
 
-    // Add gravity to the bird to make it fall
-    this.bird.body.gravity.y = 1000;
+        // Load the jump sound
+        game.load.audio('jump', 'assets/fbird/jump.wav');
+    },
 
-    // Call the 'jump' function when the spacekey is hit
-    var spaceKey = game.input.keyboard.addKey(
-                    Phaser.Keyboard.SPACEBAR);
-    spaceKey.onDown.add(this.jump, this);
-  },
+    create: function() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  update: function() {
-    // If the bird is out of the screen (too high or too low)
-    // Call the 'restartGame' function
-    if (this.bird.y < 0 || this.bird.y > 490)
-        this.restartGame();
-  },
+        this.pipes = game.add.group();
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
 
-  jump: function() {
-    // Add a vertical velocity to the bird
-    this.bird.body.velocity.y = -350;
-  },
+        this.bird = game.add.sprite(100, 245, 'bird');this.bird.scale.setTo(0.2,0.2);;
+        game.physics.arcade.enable(this.bird);
+        this.bird.body.gravity.y = 1000;
 
-// Restart the game
-  restartGame: function() {
-    // Start the 'main' state, which restarts the game
-    game.state.start('main');
-  },
+        // New anchor position
+        this.bird.anchor.setTo(-0.2, 0.5);
+
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jump, this);
+        game.input.onDown.add(this.jump, this);
+
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
+
+        // Add the jump sound
+        this.jumpSound = game.add.audio('jump');
+        this.jumpSound.volume = 0.2;
+    },
+
+    update: function() {
+        if (this.bird.y < 0 || this.bird.y > game.world.height)
+            this.restartGame();
+
+        game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
+
+        // Slowly rotate the bird downward, up to a certain point.
+        if (this.bird.angle < 20)
+            this.bird.angle += 1;
+    },
+
+    jump: function() {
+        // If the bird is dead, he can't jump
+        if (this.bird.alive == false)
+            return;
+
+        this.bird.body.velocity.y = -350;
+
+        // Jump animation
+        game.add.tween(this.bird).to({angle: -20}, 100).start();
+
+        // Play sound
+        this.jumpSound.play();
+    },
+
+    hitPipe: function() {
+        // If the bird has already hit a pipe, we have nothing to do
+        if (this.bird.alive == false)
+            return;
+
+        // Set the alive property of the bird to false
+        this.bird.alive = false;
+
+        // Prevent new pipes from appearing
+        game.time.events.remove(this.timer);
+
+        // Go through all the pipes, and stop their movement
+        this.pipes.forEach(function(p){
+            p.body.velocity.x = 0;
+        }, this);
+    },
+
+    restartGame: function() {
+        game.state.start('main');
+    },
+
+    addOnePipe: function(x, y) {
+        var pipe = game.add.sprite(x, y, 'pipe');
+        this.pipes.add(pipe);
+        game.physics.arcade.enable(pipe);
+
+        pipe.body.velocity.x = -200;
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+
+    addRowOfPipes: function() {
+        var hole = Math.floor(Math.random()*5)+1;
+
+        for (var i = 0; i < 8; i++)
+            if (i != hole && i != hole +1)
+                this.addOnePipe(400, i*60+10);
+
+        this.score += 1;
+        this.labelScore.text = this.score;
+    },
 };
+
 
 var game = new Phaser.Game(400, 490);
 
